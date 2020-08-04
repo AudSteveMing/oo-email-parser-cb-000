17405ab9c3f40f574cb00c44679f17e539ab772b
Steve Cordova <cordovas@bc.edu>
2020-08-04 19:32:22 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,11 +3,11 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-email_addresses = "john@doe.com, person@somewhere.org"
-parser = EmailAddressParser.new(email_addresses)
-
-parser.parse
-# => ["john@doe.com", "person@somewhere.org"]
+# email_addresses = "john@doe.com, person@somewhere.org"
+# parser = EmailAddressParser.new(email_addresses)
+#
+# parser.parse
+# # => ["john@doe.com", "person@somewhere.org"]
 
 class EmailAddressParser
   def initialize(email_addresses)
